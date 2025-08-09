842d357fe040d23e39e8ae54415347cee5b295fe
Aayush Ranjan <aayushranjan@Aayushs-MacBook-Air.local>
2025-08-09 14:38:33 +0530

    react-icon

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,10 +1,14 @@
 import spotifyLogo from '../../assets/Images/spotifyLogo.png'
+import { GoHomeFill } from "react-icons/go";
 
 const Navbar = () => {
   return (
-   <nav className='bg-black'>
+   <nav className='bg-black h-[60px] border'>
+   <div className= 'w-[35px] m-3 ml-6 cursor-pointer'>
+     <img src={spotifyLogo} alt="logo"/>
+   </div>
    <div>
-     {/* <img src={spotifyLogo} alt="logo"/> */}
+    <GoHomeFill/>
    </div>
    </nav>
   )
